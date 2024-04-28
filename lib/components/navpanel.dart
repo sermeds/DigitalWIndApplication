688c9285a6605e0fffc9d75bb3d6f21b4f84ef95
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-04-28 16:09:55 +0400

    панель навигации

---
+++ lib/components/navpanel.dart
@@ -0,0 +1,35 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:digital_wind_application/app_router.dart';
+import 'package:flutter/material.dart';
+
+class NavPanel extends StatelessWidget{
+
+  const NavPanel({super.key, required this.index});
+
+  final int index;
+
+  // TODO: Поставить потом нормальные ссылки
+  final List<PageRouteInfo> _routes = const [RegistrationRoute(), RegistrationRoute(), Testing(), RegistrationRoute(), RegistrationRoute()];
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items: const [
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: "Главная"),
+      BottomNavigationBarItem(icon: Icon(Icons.person), label: "Профиль"),
+      BottomNavigationBarItem(icon: Icon(Icons.play_arrow), label: "Игры и тесты"),
+      BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: "Магазин"),
+      BottomNavigationBarItem(icon: Icon(Icons.library_books), label: "Библиотека"),
+    ],
+    
+    currentIndex: index,
+    selectedItemColor: Colors.amber[800],
+    onTap: (index){
+      if(index != this.index)
+      {
+        context.router.popAndPush(_routes[index]);
+      }
+    },
+    );
+  }
+  
+}
